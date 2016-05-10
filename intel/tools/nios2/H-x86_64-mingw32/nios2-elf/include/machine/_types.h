fbfaa190d287549b49d90954150961b47e17da0e
Swee Hua Law <sweehua81@gmail.com>
2016-05-10 11:46:20 +0800

    adding nios2 gnu tools,no more seperate Quartus installation anymore.

---
+++ intel/tools/nios2/H-x86_64-mingw32/nios2-elf/include/machine/_types.h
@@ -0,0 +1,8 @@
+/*
+ *  $Id: //acds/rel/15.1/ip/sopc/app/gnu/src/newlib-2.1/newlib/libc/include/machine/_types.h#1 $
+ */
+
+#ifndef _MACHINE__TYPES_H
+#define _MACHINE__TYPES_H
+#include <machine/_default_types.h>
+#endif
